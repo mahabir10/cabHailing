9bb4e31b439202daa584c4dde8a35041960ad7aa
mahabir10 <shibam.senapati@gmail.com>
2023-11-27 14:03:53 +0530

    Removed demo project and added WalletController

--- wallet/src/main/java/com/cabhailing/wallet/WalletApplication.java
+++ wallet/src/main/java/com/cabhailing/wallet/WalletApplication.java
@@ -2,19 +2,13 @@ package com.cabhailing.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class WalletApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WalletApplication.class, args);
-
-		/*
-		 * Here we have to read the testcase file. in that testcase file we will be given the 
-		 * customer ids and the balance they posses.
-		 * So we have to create a list of customers to represent them.
-		*/
-
 	}
 
 }
---
+++ wallet/src/main/java/com/cabhailing/wallet/WalletController.java
@@ -0,0 +1,13 @@
+package com.cabhailing.wallet;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WalletController {
+    
+
+    public WalletController(){
+        
+    }
+
+}
